3300c64d612d8a0713167f33b94936fc0ca19c20
BornSava9e <Parasanand1999@gmail.com>
2023-01-04 00:07:50 +0530

    deleteapi

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,59 +12,9 @@ router.post("/blogs", BlogController.blogs)
 
 router.get("/Blogs", BlogController.getBlogs)
 
-<<<<<<< HEAD
-router.put("/blogs/:blogId", async function(req,res){
-   try{
-    let id = req.params.blogId
-    console.log(id)
-  
-   
-    // if(validObjectID(id)) {                                                  // having problem in this
-    //     res.status(404).send({msg : "object id is not correct"})}          // blog id is correct or not
-    
-    let data = req.body                                             // accessing req.body
-    let dbdata = await BlogModel.findOne({_id : id})                // finding blog with the help of id in path params
-    console.log(data.isDeleted)
-    if(data.isDeleted){                                      // if req.body has isdeleted key so create timestamps in dbdata                                                  
-        dbdata.deletedAt = Date.now()
-        
-    }
-    else if(data.isPublished){                              // if req.body has ispublished ket so create timestamps in dbdata
-    console.log(data.isPublished)
-    dbdata.publishedAt = Date.now()
-    console.log(dbdata)
-    }
-    else if(data.tags||data.subcategory){                                    // if req.body has tags and subcategory  push below content in these dbdata  
-        let tags = dbdata.tags
-        let subcat = dbdata.subcategory
-        
-        tags.push(data.tags)                                                   // adding null problem
-        subcat.push(data.subcategory)     
 
-        let update1 = await BlogModel.findOneAndUpdate(             // insert all the data inside original data                            
-            {_id : id},
-            {$set : dbdata},
-            {new :true}
-        )
-        res.status(200).send({status : true, data : update1})
-    }
-    else{                              
-        let update = await BlogModel.findOneAndUpdate(              // else if doesn't find any of the above attributes so just simpily update body data in model
-            {_id : id },
-            {$set : data},
-            {new : true})
-        
-            res.status(200).send({status : true, data : update})
-        
-    }
-   }catch(err){
-     res.status(500).send({error : "error" , msg : "something went wrong" })
-   }
-})
-=======
 router.put("/putBlogs/:blogId", BlogController.updateBlog)
 router.put("/putBlogss/:blogId", BlogController.putApi)
->>>>>>> 5849113e25c12e1154f0a6b2c2b3766a4c7331f5
 
 // DELETE /blogs?queryParams
 // Delete blog documents by category, authorid, tag name, subcategory name, unpublished
