6c2f8d91d0d979a88c3670abb7622d2c889ff561
Sanhil143 <sanhilrai143@gmail.com>
2023-01-03 14:58:02 +0530

    add

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -4,8 +4,19 @@ const mongoose = require('mongoose')
 
 
 
+//Create a blog document from request body. Get authorId in request body only.
+
+// Make sure the authorId is a valid authorId by checking the author exist in the authors collection.
+
+// Return HTTP status 201 on a succesful blog creation. Also return the blog document. The response should be a JSON object like this
+
+// Create atleast 5 blogs for each author
+
+// Return HTTP status 400 for an invalid request with a response body like this
+
 
 const blogs = async (req, res) => {
+
       try {
             let data = req.body
             let authorId = data.authorId
@@ -28,8 +39,17 @@ const blogs = async (req, res) => {
 
 
 // Get Api
+// Returns all blogs in the collection that aren't deleted and are published
+// Return the HTTP status 200 if any documents are found. The response structure should be like this
+// If no documents are found then return an HTTP status 404 with a response like this
+// Filter blogs list by applying filters. Query param can have any combination of below filters.
+// By author Id
+// By category
+// List of blogs that have a specific tag
+// List of blogs that have a specific subcategory example of a query url: blogs?filtername=filtervalue&f2=fv2
 
 const getBlogs = async (req, res) => {
+
       try {
             let data = req.query
 
@@ -97,6 +117,13 @@ const getBlogs = async (req, res) => {
       }
 
 }
+
+// PUT /blogs/:blogId
+// Updates a blog by changing the its title, body, adding tags, adding a subcategory. (Assuming tag and subcategory received in body is need to be added)
+// Updates a blog by changing its publish status i.e. adds publishedAt date and set published to true
+// Check if the blogId exists (must have isDeleted false). If it doesn't, return an HTTP status 404 with a response body like this
+// Return an HTTP status 200 if updated successfully with a body like this
+// Also make sure in the response you return the updated blog document.
 
 
 module.exports.blogs = blogs
